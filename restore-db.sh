5828c395ab7d0ca3c688c21fd8643e944b096383
Radu Salagean <radusalagean@hotmail.com>
2026-01-03 20:17:32 +0200

    fix restore script

--- restore-db.sh
+++ restore-db.sh
@@ -21,6 +21,6 @@ echo "Stopping jservice..."
 docker compose stop jservice
 
 echo "Restoring database from $BACKUP_FILE ..."
-docker exec jservice-db sh -c "PGPASSWORD=\$(cat \$POSTGRES_PASSWORD_FILE) pg_restore -U \$POSTGRES_USER -h \$POSTGRES_HOST -d \$POSTGRES_DB --clean --if-exists -F c \"$BACKUP_FILE\""
+docker exec jservice-db sh -c "PGPASSWORD=\$(cat \$POSTGRES_PASSWORD_FILE) pg_restore -U \$POSTGRES_USER -h \$POSTGRES_HOST -d \$POSTGRES_DB --clean --if-exists -F c \"/backups/$BACKUP_FILE\""
 
 echo "Restore complete."
